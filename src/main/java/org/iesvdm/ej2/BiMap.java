5981f33611a17d0fb842538559f0a83b65284305
Dani Clavijo <daniclavijonunez@gmail.com>
2024-11-10 15:09:07 +0100

    Ejercicio2

---
+++ src/main/java/org/iesvdm/ej2/BiMap.java
@@ -0,0 +1,64 @@
+package org.iesvdm.ej2;
+
+import java.util.*;
+
+public class BiMap<K, V> {
+
+    private Map<K, V> mapa = new HashMap<>();
+
+    public Map<K,V> getMapa() {
+        return mapa;
+    }
+
+    public void setMapa(Map<K,V> mapa) {
+        this.mapa = mapa;
+    }
+
+    public BiMap(Map<K, V> mapa) {
+        this.mapa = mapa;
+    }
+
+    public void put(K key, V value) {
+        if (!mapa.containsValue(value)) {
+            if (mapa.containsKey(key)) {
+                mapa.replace(key, value);
+                System.out.println("Valor reemplazado -> " + mapa);
+            } else {
+                mapa.put(key, value);
+                System.out.println("Clave y Valor añadidos al mapa -> " + mapa);
+            }
+        } else  {
+            System.out.println("No puedes introducir ese valor, ya corresponde a una clave -> " + mapa);
+        }
+    }
+
+    public void forcePut(K key, V value) {
+        List<V> values = new ArrayList<>(mapa.values());
+
+        for (int i = 0; i < values.size(); i++) {
+            if (values.get(i).equals(value)) {
+                mapa.remove(mapa.keySet().stream().toList().get(i));
+                mapa.put(key, value);
+            }
+        }
+
+        System.out.println("Clave y Valor forzados al mapa -> " + mapa);
+    }
+
+    public void inv() {
+        Map<V, K> m1 = new HashMap<>();
+
+        Set<K> keys = mapa.keySet();
+        List<K> keysList = new ArrayList<>(keys);
+        List<V> values = new ArrayList<>();
+        values.addAll(mapa.values());
+
+        for (int i = 0; i < mapa.size(); i++) {
+            m1.put(values.get(i), keysList.get(i));
+        }
+
+        System.out.println("Mapa Invertido -> " + m1);
+
+    }
+
+}
---
+++ src/main/java/org/iesvdm/ej2/Main.java
@@ -0,0 +1,23 @@
+package org.iesvdm.ej2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Map<Integer, String> m1 = new HashMap<>();
+
+        BiMap bm1 = new BiMap(m1);
+
+        bm1.put(4, "2");
+        bm1.put(1, "3");
+        bm1.inv();
+
+        bm1.forcePut(3, "3");
+        bm1.put(4, "4");
+        bm1.put(3, "4");
+
+    }
+
+}
